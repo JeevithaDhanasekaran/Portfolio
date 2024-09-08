1a6aca9ee474381fbc64936fe17063814fc5bc3a
JeevithaDhanasekaran <jeevithasekar2127@gmail.com>
2024-09-08 18:15:05 +0530

    Check footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 export default function Footer(){
     return <div className="py-4 bg-secondary text-center text-white">
-        &copy; Jeevitha Dhanasekaran 2024
+        &copy; Jeevitha Dhanasekaran 2024New
     </div>
 }
